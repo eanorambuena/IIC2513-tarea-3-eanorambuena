0c4f845d738236f9789fee26bfad740b9f50096b
Emmanuel Norambuena <eanorambuena@uc.cl>
2024-04-27 01:32:53 -0400

    Add delete and patch

--- backend/src/routes/entries.js
+++ backend/src/routes/entries.js
@@ -2,10 +2,94 @@ const Router = require('koa-router')
 
 const router = new Router()
 
-router.get('/', async (ctx) => {
-    const entries = await ctx.orm.Entry
-        .findAll()
-    ctx.body = entries
+router.post('/', async (ctx) => {
+    const body = ctx.request.body
+    console.log(`Body: ${body}`)
+    try {
+        await ctx.orm.Entry
+            .create(body)
+        ctx.status = 201
+    }
+    catch (error) {
+        ctx.body = { error: error.message }
+        ctx.status = 500
+    }
 })
+
+router.get('/', async (ctx) => {
+    try {
+        const entries = await ctx.orm.Entry
+            .findAll()
+        ctx.body = entries
+    }
+    catch (error) {
+        ctx.body = { error: error.message }
+        ctx.status = 500
+    }
+})
+
+
+router.get('/user/:username', async (ctx) => {
+    const username = ctx.params.username
+    try {
+        const entries = await ctx.orm.Entry
+            .findAll({where: {belongs_to: username}})
+        ctx.body = entries
+    }
+    catch (error) {
+        ctx.body = { error: error.message }
+        ctx.status = 500
+    }
+})
+
+router.get('/:id', async (ctx) => {
+    const id = ctx.params.id
+    try {
+        const entry = await ctx.orm.Entry
+            .findByPk(id)
+        if (entry) {
+            ctx.body = entry
+            ctx.status = 200
+        }
+        else {
+            ctx.body = { error: 'Entry not found' }
+            ctx.status = 404
+        }
+    }
+    catch (error) {
+        ctx.body = { error: error.message }
+        ctx.status = 500
+    }
+})
+
+router.patch('/:id', async (ctx) => {
+    const id = ctx.params.id
+    const body = ctx.request.body
+    try {
+        const entry = await ctx.orm.Entry
+            .findByPk(id)
+        await entry.update(body)
+        ctx.body = entry
+    }
+    catch (error) {
+        ctx.body = { error: error.message }
+        ctx.status = 500
+    }
+})
+
+router.delete('/:id', async (ctx) => {
+    const id = ctx.params.id
+    try {
+        const entry = await ctx.orm.Entry
+            .findByPk(id)
+        await entry.destroy()
+        ctx.status = 204
+    }
+    catch (error) {
+        ctx.body = { error: error.message }
+        ctx.status = 500
+    }
+})
+
 
 module.exports = router
--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -2,24 +2,6 @@ const Router = require('koa-router')
 
 const router = new Router()
 
-router.get('/:username', async (ctx) => {
-    try {
-        const user = await ctx.orm.User
-            .findByPk(ctx.params.username)
-        console.log(`User: ${user}`)
-        if (user) {
-            ctx.body = user
-            ctx.status = 200
-        } else {
-            ctx.status = 404
-        }
-    }
-    catch (error) {
-        console.log(error)
-        ctx.status = 500
-    }
-})
-
 router.post('/', async (ctx) => {
     const body = ctx.request.body
     const user = {username: body.username}
@@ -33,5 +15,37 @@ router.post('/', async (ctx) => {
         ctx.status = 500
     }
 })
+
+router.get('/', async (ctx) => {
+    try {
+        const users = await ctx.orm.User
+            .findAll()
+        ctx.body = users
+    }
+    catch (error) {
+        console.log(error)
+        ctx.status = 500
+    }
+})
+
+router.get('/:username', async (ctx) => {
+    try {
+        const user = await ctx.orm.User
+            .findByPk(ctx.params.username)
+        console.log(`User: ${user}`)
+        if (user) {
+            ctx.body = user
+            ctx.status = 200
+        } else {
+            ctx.body = { error: 'User not found' }
+            ctx.status = 404
+        }
+    }
+    catch (error) {
+        ctx.body = { error: error.message }
+        ctx.status = 500
+    }
+})
+
 
 module.exports = router
